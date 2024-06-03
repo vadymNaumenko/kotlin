1d816aa48ced2f4a932d467d9b41a8e60f850098
Vdym <vadimnaumnaum@gmail.com>
2024-06-03 23:31:39 +0200

    kotlin

---
+++ basic/basicKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+
+    val constNumber = 25;
+    var str = "Hello World";
+
+    println(str);
+    println(constNumber);
+
+    // целочисленные
+    val intNumber: Int = 32 // 32 bit
+    val longNumber: Long = 64  // 64 bit
+    val shortNumber: Short = 16 // 16 bit
+    val byteNumber : Byte = 8 // bit
+    val unsignedNumber:UInt = 21u // 32 bit
+
+    // веществиные
+    val doubleNumber: Double = 234.6; // 64 bit
+    val floatNumber: Float  = 432.6f; // 32 bit
+
+}
